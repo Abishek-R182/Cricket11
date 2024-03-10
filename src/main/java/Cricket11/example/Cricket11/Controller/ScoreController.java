0ae32030ebd9372adbb49bea4d5fd8c42403b51a
NICEDAY <abishekr182@gmail.com>
2024-03-10 14:45:34 +0530

    Updated API in this Controller

--- src/main/java/Cricket11/example/Cricket11/Controller/ScoreController.java
+++ src/main/java/Cricket11/example/Cricket11/Controller/ScoreController.java
@@ -2,21 +2,28 @@ package Cricket11.example.Cricket11.Controller;
 
 import Cricket11.example.Cricket11.Entity.Results;
 import Cricket11.example.Cricket11.Service.ScoreService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-@RestController
+@Controller
 public class ScoreController {
     @Autowired
     ScoreService scoreService;
 
     @GetMapping("/getresult")
-    public List<Results> fetchScore(){
-        List<Results> r = scoreService.calculateScore();
-        return r;
+    public String fetchScore(Model model){
+
+        List<Results> playerResults = scoreService.calculateScore();
+        model.addAttribute("playerResults",playerResults);
+        return "PlayerResultView";
     }
+
+
 }
