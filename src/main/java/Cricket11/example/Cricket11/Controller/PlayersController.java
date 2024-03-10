163c145adc4878d50459077c8bb83dc185b439da
NICEDAY <abishekr182@gmail.com>
2024-03-10 21:34:27 +0530

    Updated API in this Controller

--- src/main/java/Cricket11/example/Cricket11/Controller/PlayersController.java
+++ src/main/java/Cricket11/example/Cricket11/Controller/PlayersController.java
@@ -1,14 +1,19 @@
 package Cricket11.example.Cricket11.Controller;
 
 import Cricket11.example.Cricket11.Entity.Players;
+import Cricket11.example.Cricket11.Entity.Results;
+import Cricket11.example.Cricket11.Repositry.ResultRepo;
 import Cricket11.example.Cricket11.Service.PlayersService;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @Slf4j
 public class PlayersController {
     @Autowired
@@ -19,6 +24,19 @@ public class PlayersController {
         List<Players> players = playersService.getAllPlayers();
         return players;
     }
+
+    @Autowired
+    private ResultRepo resultRepo;
+
+    @GetMapping("/getresults")
+    public String getAllEntities(Model model) {
+        List<Results> entities = resultRepo.findAll();
+        model.addAttribute("playerResults", entities);
+        return "PlayerResultView";
+    }
+
+
+
 
     @PostMapping("/addplayers")
     public Players postPlayers(@RequestBody Players p){
--- src/main/java/Cricket11/example/Cricket11/Controller/UserController.java
+++ src/main/java/Cricket11/example/Cricket11/Controller/UserController.java
@@ -1,11 +1,17 @@
 package Cricket11.example.Cricket11.Controller;
 
+import Cricket11.example.Cricket11.Entity.Results;
 import Cricket11.example.Cricket11.Entity.Userdetails;
+import Cricket11.example.Cricket11.Repositry.ResultRepo;
 import Cricket11.example.Cricket11.Service.PlayersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -17,5 +23,14 @@ public class UserController {
     public Userdetails postUser(@RequestBody Userdetails k){
         Userdetails u = playersService.postUserdetails(k);
         return u;
+    }
+
+    @Autowired
+    private ResultRepo resultRepo;
+    @GetMapping("/getuser")
+    public String getUser(Model model){
+        List<Results> userName = resultRepo.findAll();
+        model.addAttribute("",userName);
+        return "PlayerResultView";
     }
 }
